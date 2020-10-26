affdaf89b6935740579eb6951427c15502de3a92
Your Name <your@email.com>
2020-10-26 22:28:03 +0000

    es  eslinted

--- assertEqual.js
+++ assertEqual.js
@@ -2,9 +2,12 @@
 const assertEqual = function(actual, expected) {
   // console.assert(actual === expected);
   if (actual === expected) {
-    console.log("Assertion Passed: " + actual + " === " + expected);
+    console.log(`Assertion Pased: ${actual} === ${expected}`);
+    // console.log("Assertion Passed: " + actual + " === " + expected);
   } else {
-    console.log("Assertion Failed: " + '"' + actual + '"' + " === " + '"' + expected + '"');
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+
+    // console.log("Assertion Failed: " + '"' + actual + '"' + " !== " + '"' + expected + '"');
   }
 };
 
