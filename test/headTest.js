ba2598f6613a1788fdfd18a7361784e1559151db
Your Name <your@email.com>
2020-11-02 21:13:22 +0000

    add test/headTest.js

---
+++ test/headTest.js
@@ -0,0 +1,16 @@
+const chai = require('chai');
+const assert = chai.assert;
+const assertEqual = require('../assertEqual');
+const head = require('../head');
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+  assert.strictEqual(head(['5']), '5');
+});
+
+
+});
