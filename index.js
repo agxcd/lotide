7e8e43d18ef3fc6192d9f69da84255a1f1d0702c
Your Name <your@email.com>
2020-11-03 01:57:13 +0000

    shortening index.js

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const head = require('./head');
 const tail = require('./tail');
 const middle = require('./middle');
+const countLetters = require('./countLetters');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
 
 module.exports = {
-  head: head,
-  tail: tail,
-  middle: middle
+  head,
+  tail,
+  middle,
+  countLetters,
+  countLetters,
+  countOnly,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  flatten,
+  map,
+  takeUntil,
+  without
 };
